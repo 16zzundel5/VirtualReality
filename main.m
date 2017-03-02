82fdf642cdb08dc31cd3b48d639d41ea8126c68d
Zach Zundel <zach.zundel@utah.edu>
2017-03-02 02:00:42 -0700

    Added main.m

---
+++ main.m
@@ -0,0 +1,10 @@
+%% Virtual Reality Renderer for MATLAB
+%  BIOEN 3301 Final Project
+%  Patrick Pearson, Julie Tang, and Zach Zundel
+
+while(true)
+    % Retrieve image
+    % Get calibration data
+    % Render sphere
+    % Display result
+end
