0e00e1590ad99325f686bea0de760a95822d100a
Zach Zundel <zach.zundel@utah.edu>
2017-03-03 22:48:32 -0700

    Added first pass at checkerboard detection

--- main.m
+++ main.m
@@ -2,9 +2,44 @@
 %  BIOEN 3301 Final Project
 %  Patrick Pearson, Julie Tang, and Zach Zundel
 
-while(true)
+%%
+clear('cam');
+
+%% Set up webcam
+cam = webcam;
+
+%% Set up video writer
+writer = VideoWriter('checkerboard.avi', 'MPEG-4');
+open(writer)
+
+%% Main VR loop
+
+% First write 30 frames of video with no checkerboard detection
+for k = 1:30
+  writeVideo(writer, snapshot(cam)) 
+end
+
+% Now do checkerboard for 30 frames
+for k = 1:30;
     % Retrieve image
+    img = snapshot(cam);
+         
     % Get calibration data
+    [imagePoints, boardSize] = detectCheckerboardPoints(img);
+    
+    hold on;
+    if ~isempty(imagePoints)
+        imagePoints(:,3) = 10;
+        img = insertShape(img, 'FilledCircle', imagePoints, 'Color', 'green', 'Opacity', 0.7);
+    end
+    
+    imshow(img)
+    
+    writeVideo(writer, img);
+    
     % Render sphere
     % Display result
 end
+
+clear('cam');
+close(writer);
