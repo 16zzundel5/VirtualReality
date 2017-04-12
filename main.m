5f9e7ba594b0fb894fb99a7fc2843e0cb7806804
Zach Zundel <zach.zundel@utah.edu>
2017-04-12 15:28:40 -0600

    Zach's drawing

--- main.m
+++ main.m
@@ -2,8 +2,8 @@
 %  BIOEN 3301 Final Project
 %  Patrick Pearson, Julie Tang, and Zach Zundel
 
-img = imread('test.jpg');
-gifOut = 'output.gif';
+for imgName = 1:1
+img = imread(['small calibration data/' num2str(imgName) '.jpg']);
 
 [imagePoints, boardSize] = detectCheckerboardPoints(img);
 
@@ -15,27 +15,29 @@ worldPoints = [[a,a,a,a,a,a,a,a,a]',[-4:4,-4:4,-4:4,-4:4,-4:4,-4:4,-4:4]'];
 
 [rotation, translation] = extrinsics(imagePoints, worldPoints, cameraParams);
 
-L = 5;
 
-for L = 5:15
+    sphere_center = worldToImage(cameraParams, rotation, translation, [0 0 0]);
     [X Y Z] = sphere(L);
-
+    
     x = reshape(X, 1, (L + 1) * (L + 1));
     y = reshape(Y, 1, (L + 1) * (L + 1));
     z = reshape(Z, 1, (L + 1) * (L + 1));
 
-    sphere_coords = [x' y' z'];
+    sphere_coords = bsxfun(@plus, rotation^-1 * [x; y; z], translation');
+    
+    X = reshape(sphere_coords(1, :), L + 1, L + 1);
+    Y = reshape(sphere_coords(2, :), L + 1, L + 1);
+    Z = reshape(sphere_coords(3, :), L + 1, L + 1);  
+   
+    X = X * pixels_per_inch + sphere_center(1);
+    Y = Y * pixels_per_inch + sphere_center(2);
+    %Z = (Z + 2) * pixels_per_inch;
+   
+    figure
+    ax = gca;
+    
+    imagesc(img, 'Parent', ax);
+    hold on
+    surf(X, Y , Z, 'Parent', ax);
 
-    spherePoints = [worldToImage(cameraParams, rotation, translation, sphere_coords) 30*ones((L + 1) * (L + 1),1)];
-    img_L = insertShape(img, 'FilledCircle', spherePoints, 'Color', 'red');
-    
-    image(img_L)
-    
-    [imind, cm] = rgb2ind(img_L, 256);
-    
-    if L == 5
-        imwrite(imind, cm, gifOut, 'gif', 'LoopCount', Inf, 'DelayTime', 1);
-    else
-        imwrite(imind, cm, gifOut, 'gif', 'WriteMode', 'append', 'DelayTime', 1);
-    end
 end
