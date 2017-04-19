820a0353e7d6f347f448e6045f83d964f9d96fe2
Zach Zundel <zach.zundel@utah.edu>
2017-04-19 16:27:14 -0600

    Added position interpolation

--- main.m
+++ main.m
@@ -7,11 +7,14 @@ cam = webcam;
 
 frames = 0;
 
+samples = zeros(1, 3)
+
+
 while 1
     img = snapshot(cam);
     if mod(frames, 10) == 0
         [imagePoints, boardSize] = detectCheckerboardPoints(img);
-
+        
         pixels_per_inch = sqrt((imagePoints(1,1) - imagePoints(2,1))^2 ...
                              + (imagePoints(1,2) - imagePoints(2,2))^2);
 
@@ -19,18 +22,26 @@ while 1
         worldPoints = [x(:), y(:)];   
 
         [rotation, translation] = extrinsics(imagePoints, worldPoints, cameraParams);
+
+        sampleTimes = 10 * (0:(frames / 10));
+        samples(frames/10 + 1, :) = translation;
+    end
+    
+    if frames > 20
+        interpolatedPosition = interp1(sampleTimes, samples, frames, 'pchip', 'extrap');
+    else
+        interpolatedPosition = translation;
     end
 
     [X Y Z] = sphere(10);
 
     fvc = surf2patch(X * 2, Y * 2, Z * 2);
 
-    vertices = worldToImage(cameraParams, rotation, translation, fvc.vertices);
+    vertices = worldToImage(cameraParams, rotation, interpolatedPosition, fvc.vertices);
 
     imagesc(img);
     hold on
     patch('Faces', fvc.faces, 'Vertices', vertices, 'FaceAlpha', 0, 'EdgeColor', 'green');
     hold off
-
     frames = frames + 1;
 end
