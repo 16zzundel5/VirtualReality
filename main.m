3f670dbc669fb8fc1d855435c39995cffef0246a
Zach Zundel <zach.zundel@utah.edu>
2017-04-30 13:16:20 -0600

    Added GUI

--- main.m
+++ main.m
@@ -11,14 +11,18 @@ while 1
     img = snapshot(cam);
     if mod(frames, 10) == 0
         [imagePoints, boardSize] = detectCheckerboardPoints(img);
+        
+        if numel(imagePoints) > 0
+            pixels_per_inch = sqrt((imagePoints(1,1) - imagePoints(2,1))^2 ...
+                                 + (imagePoints(1,2) - imagePoints(2,2))^2);
 
-        pixels_per_inch = sqrt((imagePoints(1,1) - imagePoints(2,1))^2 ...
-                             + (imagePoints(1,2) - imagePoints(2,2))^2);
+            [x, y] = meshgrid(-4:4, -3:3);
+            worldPoints = [x(:), y(:)];   
 
-        [x, y] = meshgrid(-4:4, -3:3);
-        worldPoints = [x(:), y(:)];   
-
-        [rotation, translation] = extrinsics(imagePoints, worldPoints, cameraParams);
+            [rotation, translation] = extrinsics(imagePoints, worldPoints, cameraParams);
+        else
+            frames = frames - 1;
+        end
     end
 
     [X Y Z] = sphere(10);
@@ -27,7 +31,7 @@ while 1
 
     vertices = worldToImage(cameraParams, rotation, translation, fvc.vertices);
 
-    imagesc(img);
+    image(img);
     hold on
     patch('Faces', fvc.faces, 'Vertices', vertices, 'FaceAlpha', 0, 'EdgeColor', 'green');
     hold off
